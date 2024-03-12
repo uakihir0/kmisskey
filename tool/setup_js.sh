74b9d68070759ca2f1adf997bee8a6110e0e6a03
Akihiro Urushiara <a.urusihara@gmail.com>
2024-03-13 00:26:28 +0900

    wrapping type to promise.

--- tool/setup_js.sh
+++ tool/setup_js.sh
@@ -5,7 +5,7 @@ BUILD_PATH=../pods/build/dist/js/developmentLibrary
 
 # Make Ignore Files
 # cd "$BASE_PATH" || exit
-# cd $BUILD_PATH || exit
+# cd "$BUILD_PATH" || exit
 # touch .gitignore
 # echo "node_modules" >> .gitignore
 # touch .npmignore
@@ -16,3 +16,9 @@ cd "$BASE_PATH" || exit
 cp ../LICENSE $BUILD_PATH/LICENSE
 cp ../docs/js/README.md $BUILD_PATH/README.md
 cp ../docs/js/README_ja.md $BUILD_PATH/README_ja.md
+
+# Wrapping type to Promise (kmisskey-js.d.ts)
+cd "$BASE_PATH" || exit
+cd "$BUILD_PATH" || exit
+sed -i -e "s|): work\(.*\)>;|): Promise<work\1>>;|g" kmisskey-js.d.ts
+rm kmisskey-js.d.ts-e

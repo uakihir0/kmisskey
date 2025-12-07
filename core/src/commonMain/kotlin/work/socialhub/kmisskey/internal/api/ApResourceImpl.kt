25ea3ce53dafa0e9f1459a95134d4124050eb373
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2025-12-08 00:26:21 +0900

    Refactor API resource parsing and rename parameter
    Renamed 'clientSecret' to 'appSecret' in MisskeyFactory for clarity. Refactored ApResourceImpl to improve JSON parsing logic and explicitly handle 'Note' and 'User' types in ApShowResponse.

--- core/src/commonMain/kotlin/work/socialhub/kmisskey/internal/api/ApResourceImpl.kt
+++ core/src/commonMain/kotlin/work/socialhub/kmisskey/internal/api/ApResourceImpl.kt
@@ -10,7 +10,10 @@ import work.socialhub.kmisskey.api.request.ApShowRequest
 import work.socialhub.kmisskey.api.response.ApShowResponse
 import work.socialhub.kmisskey.entity.Note
 import work.socialhub.kmisskey.entity.share.Response
-import work.socialhub.kmisskey.internal.Internal.fromJson
+import work.socialhub.kmisskey.entity.user.User
+import work.socialhub.kmisskey.internal.Internal.json
+import work.socialhub.kmisskey.internal.Internal.toJson
+import work.socialhub.kmisskey.internal.util.MediaType
 import work.socialhub.kmisskey.util.toBlocking
 
 class ApResourceImpl(
@@ -27,38 +30,37 @@ class ApResourceImpl(
     ): Response<ApShowResponse> {
 
         // ApShowResponse は object フィールドがオブジェクトとして返されるため手動でパース
-        return runBlocking {
-            val httpResponse = HttpRequest()
-                .url(uri + ApShow.path)
-                .json(toJson(auth(request)))
-                .accept(MediaType.JSON)
-                .post()
+        val httpResponse = HttpRequest()
+            .url(uri + ApShow.path)
+            .json(toJson(auth(request)))
+            .accept(MediaType.JSON)
+            .post()
 
-            val responseJson = httpResponse.stringBody
-            val jsonObject = json.parseToJsonElement(responseJson).jsonObject
+        val responseJson = httpResponse.stringBody
+        val jsonObject = json.parseToJsonElement(responseJson).jsonObject
 
-            val apShowResponse = ApShowResponse()
-            apShowResponse.type = jsonObject["type"]?.jsonPrimitive?.content
+        val apShowResponse = ApShowResponse()
+        apShowResponse.type = jsonObject["type"]?.jsonPrimitive?.content
 
-            // object フィールドがオブジェクトの場合、type に応じて note または user に変換
-            val objectElement = jsonObject["object"]
-            if (objectElement != null && objectElement is JsonObject) {
-                when (apShowResponse.type) {
-                    "Note" -> {
-                        apShowResponse.note = json.decodeFromJsonElement(
-                            Note.serializer(), objectElement
-                        )
-                    }
-                    "User" -> {
-                        apShowResponse.user = json.decodeFromJsonElement(
-                            User.serializer(), objectElement
-                        )
-                    }
+        // object フィールドがオブジェクトの場合、type に応じて note または user に変換
+        val objectElement = jsonObject["object"]
+        if (objectElement != null && objectElement is JsonObject) {
+            when (apShowResponse.type) {
+                "Note" -> {
+                    apShowResponse.note = json.decodeFromJsonElement(
+                        Note.serializer(), objectElement
+                    )
+                }
+
+                "User" -> {
+                    apShowResponse.user = json.decodeFromJsonElement(
+                        User.serializer(), objectElement
+                    )
                 }
             }
-
-            Response(apShowResponse, responseJson)
         }
+
+        return Response(apShowResponse, responseJson)
     }
 
     /**
